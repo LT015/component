0e037a6da626afec1f326ed3e257aef8b685c430
liutao <1342436308@qq.com>
2019-07-15 13:28:34 +0800

    eventbus

---
+++ model_eventBus/src/main/java/com/lt/model_eventbus/EventBus.java
@@ -0,0 +1,102 @@
+package com.lt.model_eventbus;
+
+import android.net.wifi.aware.SubscribeConfig;
+import android.telephony.SubscriptionInfo;
+import android.util.Log;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import javax.security.auth.login.LoginException;
+
+public class EventBus {
+
+    private HashMap<Object, List<SubscribeMethod>> cacheMap;
+    private static volatile EventBus instance;
+
+    public static EventBus getDefault(){
+        if(instance == null){
+            instance = new EventBus();
+        }
+        return instance;
+    }
+    private EventBus(){
+        cacheMap = new HashMap<>();
+    }
+
+    public void register(Object obj) {
+        //寻找到所有object类带有subscribe注解的方法
+        List<SubscribeMethod> list = cacheMap.get(obj);
+       if(list == null){
+           list = findSubscribeMethod(obj);
+           cacheMap.put(obj,list);
+       }
+    }
+
+    private List<SubscribeMethod> findSubscribeMethod(Object obj) {
+        List<SubscribeMethod> list = new ArrayList<>();
+        Class<?> clazz = obj.getClass();
+        while(clazz != null){
+            String name = clazz.getName();
+            if(name.startsWith("java.") || name.startsWith("javax.") || name.startsWith("android.")){
+                break;
+            }
+            Method[] methods = clazz.getDeclaredMethods();
+            for(Method method : methods){
+                Subscribe subscribe = method.getAnnotation(Subscribe.class);
+                if(subscribe == null){
+                    continue;
+                }
+                //判断带有subscribe注解方法中的参数类型
+                Class<?>[] types = method.getParameterTypes();
+                if(types.length != 1){
+                    Log.d("错误", "eventbus only accpet one para: ");
+                }
+                ThreadMode threadMode = subscribe.threadMode();
+                SubscribeMethod sbm = new SubscribeMethod(method,threadMode,types[0]);
+                list.add(sbm);
+            }
+            clazz = clazz.getSuperclass();
+        }
+
+
+
+        return  list;
+
+
+    }
+
+    public void post(Object type) {
+        //直接循环cacheMap里的所有方法
+        Set<Object> set = cacheMap.keySet();
+        Iterator<Object> iterator = set.iterator();
+        while(iterator.hasNext()){
+            Object obj = iterator.next();
+            List<SubscribeMethod> list = cacheMap.get(obj);
+            for(SubscribeMethod subscribeMethod : list){ //通过传来得到参数来确定函数
+                if(subscribeMethod.getType().isAssignableFrom(type.getClass())){
+
+                    subscribeMethod.getmThreadMode();
+                    //通过mode得模式 来进行线程切换
+                    invoke(subscribeMethod,obj,type);
+                }
+            }
+        }
+    }
+    private void invoke(SubscribeMethod subscribeMethod,Object obj,Object type){
+        Method method = subscribeMethod.getmMethod();
+        try {
+            method.invoke(obj,type);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+    //要维护一个删除
+}
--- module_main/src/main/java/com/lt/module_one/MainActivity.java
+++ module_main/src/main/java/com/lt/module_one/MainActivity.java
@@ -1,13 +1,31 @@
 package com.lt.module_one;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.lt.model_eventbus.EventBean;
+import com.lt.model_eventbus.EventBus;
+import com.lt.model_eventbus.SecondActivity;
+import com.lt.model_eventbus.Subscribe;
+import com.lt.model_eventbus.ThreadMode;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EventBus.getDefault().register(this);
+        startActivity(new Intent(this, SecondActivity.class));
     }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void a(EventBean eventBean){
+        Log.d(TAG, eventBean.toString());
+    }
+
 }
